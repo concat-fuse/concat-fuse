c67d3ab6939f1dc0a41763dc3bb078d5ad71e8f3
Ingo Ruhnke <grumbel@gmail.com>
2015-02-26 21:28:00 +0100

    Added --from-file, --unmount, --keep options to vcat.py

--- vcat.py
+++ vcat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 # concat-fuse
 # Copyright (C) 2015 Ingo Ruhnke <grumbel@gmx.de>
@@ -17,53 +17,136 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+from __future__ import print_function
+
 import argparse
-import os
-import hashlib
-import subprocess
 import glob
+import hashlib
+import os
+import subprocess
+import sys
 
 
-def init_concat_fuse():
-    """Returns the name of the concat-fuse directory and mount it when needed"""
+class ConcatFuse:
 
-    runtime_base_dir = os.environ.get("XDG_RUNTIME_DIR")
-    if runtime_base_dir:
-        concat_fuse_dir = os.path.join(runtime_base_dir, "concat-fuse")
-    else:
-        concat_fuse_dir = os.path.join(os.environ["HOME"], ".concat-fuse")
+    @staticmethod
+    def unmount():
+        basedir = ConcatFuse.find_basedir()
+        if os.path.ismount(basedir):
+            subprocess.check_call(["fusermount", "-u", basedir])
 
-    if not os.path.exists(concat_fuse_dir):
-        os.mkdir(concat_fuse_dir)
+    @staticmethod
+    def find_basedir():
+        runtime_base_dir = os.environ.get("XDG_RUNTIME_DIR")
+        if runtime_base_dir:
+            basedir = os.path.join(runtime_base_dir, "concat-fuse")
+        else:
+            basedir = os.path.join(os.environ["HOME"], ".concat-fuse")
+        return basedir
 
-    if not os.path.ismount(concat_fuse_dir):
-        subprocess.check_call(["concat-fuse", concat_fuse_dir])
+    def __init__(self, basedir=None):
+        """Mounts concat-fuse to basedir when needed"""
 
-    return concat_fuse_dir
+        if basedir is None:
+            self.basedir = ConcatFuse.find_basedir()
+        else:
+            self.basedir = basedir
+
+        if not os.path.exists(self.basedir):
+            os.mkdir(self.basedir)
+
+        if not os.path.ismount(self.basedir):
+            subprocess.check_call(["concat-fuse", self.basedir])
+
+    def concat(self, files):
+        """Send the file list to concat-fuse and return the filename of the virtual file"""
+
+        # write the file list to concat-fuse
+        files_serialized = "\0".join(files)
+        with open(os.path.join(self.basedir, "from-file0/control"), "wb") as fout:
+            fout.write(files_serialized)
+
+        # return the virtual file name
+        multifile_id = hashlib.sha1(files_serialized).hexdigest()
+        virtual_file = os.path.join(self.basedir, "from-file0", multifile_id)
+        return virtual_file
+
+
+def splitlines0(data):
+    lst = data.split("\0")
+    if lst and lst[-1] == "":
+        del lst[-1]
+    return lst
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Create virtual concatenated files from a glob pattern",
+    parser = argparse.ArgumentParser(description="Create virtual concatenated files.",
                                      epilog="The name of the virtual file is returned on stdout.")
-    parser.add_argument('GLOB', action='store', nargs='+', help="A glob pattern")
+
+    # argparser does not allow freely mixing optional and positional
+    # arguments, this limits the functionality of `vcat` somewhat, but
+    # shouldn't be to critical: http://bugs.python.org/issue14191
+
+    concat_fuse_group = parser.add_argument_group("concat-fuse options")
+    concat_fuse_group.add_argument('-u', '--unmount', action='store_true', help="Unmount concat-fuse")
+
+    files_group = parser.add_argument_group("file options")
+    files_group.add_argument('-n', '--dry-run', action='store_true', help="Don't send file list to concat-fuse, print to stdout")
+    files_group.add_argument('-k', '--keep', action='store_true', help="Continue despite errors")
+    files_group.add_argument('FILES', nargs='*', help="Files to virtually concat")
+    files_group.add_argument('--from-file', metavar='FILE', action='append', default=[], help="Read files from FILE, newline separated")
+    files_group.add_argument('--from-file0', metavar='FILE', action='append', default=[], help="Read files from FILE, '\\0' separated")
+    # files_group.add_argument('--glob', metavar='GLOB', help="Read files from FILE, '\\0' separated")
+
+    stdin_group = files_group.add_mutually_exclusive_group()
+    stdin_group.add_argument('-', dest="stdin", action='store_true', help="Read files from stdin")
+    stdin_group.add_argument('-0', dest="stdin0", action='store_true', help="Read files from stdin, '\\0' separated")
+
     args = parser.parse_args()
 
-    concat_fuse_dir = init_concat_fuse()
+    if args.unmount:
+        ConcatFuse.unmount()
+    else:
+        # generate the file list
+        files = []
 
-    # generate the file list
-    files = []
-    for g in args.GLOB:
-        files.extend([os.path.abspath(p) for p in glob.glob(g)])
+        if args.stdin:
+            files.extend(sys.stdin.read().splitlines())
+        elif args.stdin0:
+            files.extend(splitlines0(sys.stdin.read()))
 
-    # write the file list to concat-fuse
-    files_serialized = "\n".join(files)
-    with open(os.path.join(concat_fuse_dir, "from-file/control"), "wb") as fout:
-        fout.write(files_serialized)
+        for f in args.from_file:
+            with open(f, "r") as fin:
+                files.extend(fin.read().splitlines())
 
-    # return the virtual file name
-    multifile_id = hashlib.sha1(files_serialized).hexdigest()
-    virtual_file = os.path.join(concat_fuse_dir, "from-file", multifile_id)
-    print virtual_file
+        for f in args.from_file0:
+            with open(f, "rb") as fin:
+                files.extend(splitlines0(fin.read()))
+
+        files.extend(args.FILES)
+
+        files = [os.path.abspath(f) for f in files]
+
+        errors = False
+        for f in files:
+            if not os.path.exists(f):
+                print("%s: No such file or directory" % f, file=sys.stderr)
+                errors = True
+
+        if errors and not args.keep:
+            sys.exit(1)
+
+        if not files:
+            print("%s: fatal error: no input files provided" % sys.argv[0], file=sys.stderr)
+            sys.exit(1)
+        else:
+            if args.dry_run:
+                for f in files:
+                    print(f)
+            else:
+                concat_fuse = ConcatFuse()
+                virtual_filename = concat_fuse.concat(files)
+                print(virtual_filename)
 
 
 if __name__ == "__main__":
