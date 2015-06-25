a53a41c1bbf3f6f679f0fee01c222efee1466b49
Ingo Ruhnke <grumbel@gmail.com>
2015-06-25 20:32:38 +0200

    Replaced grumbel@gmx.de with grumbel@gmail.com

--- cfconcat.py
+++ cfconcat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # concat-fuse
-# Copyright (C) 2015 Ingo Ruhnke <grumbel@gmx.de>
+# Copyright (C) 2015 Ingo Ruhnke <grumbel@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
